6b69db44b757d5046f144731592b161bb6ced750
rochapbernardo <bernardopolese2809@gmail.com>
2025-07-26 12:11:03 -0300

    add GetMainClassCommand

--- app/src/main/java/io/github/syntaxpresso/core/command/JavaCommand.java
+++ app/src/main/java/io/github/syntaxpresso/core/command/JavaCommand.java
@@ -1,10 +1,11 @@
 package io.github.syntaxpresso.core.command;
 
 import io.github.syntaxpresso.core.command.java.CreateNewJavaFileCommand;
+import io.github.syntaxpresso.core.command.java.GetMainClassCommand;
 import picocli.CommandLine.Command;
 
 @Command(
     name = "java",
     description = "Parent command for all Java-specific operations.",
-    subcommands = {CreateNewJavaFileCommand.class})
+    subcommands = {CreateNewJavaFileCommand.class, GetMainClassCommand.class})
 public class JavaCommand {}
