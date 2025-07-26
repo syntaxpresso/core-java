3eb0791db09e3da23c2ff08e7b080315647ca615
André Luís <zandreluisos@gmail.com>
2025-07-26 17:16:05 -0300

    Create CommandFactory to decouple service layer

--- app/src/main/java/io/github/syntaxpresso/core/Core.java
+++ app/src/main/java/io/github/syntaxpresso/core/Core.java
@@ -1,12 +1,18 @@
 package io.github.syntaxpresso.core;
 
+import io.github.syntaxpresso.core.util.CommandFactory;
 import io.github.syntaxpresso.core.command.GenericCommand;
 import io.github.syntaxpresso.core.command.JavaCommand;
 import picocli.CommandLine;
 
-@CommandLine.Command(subcommands = {JavaCommand.class, GenericCommand.class})
+@CommandLine.Command(
+    subcommands = {
+      JavaCommand.class,
+      GenericCommand.class,
+    })
 public class Core {
   public static void main(String[] args) {
-    System.exit(new CommandLine(new Core()).execute(args));
+    CommandLine commandLine = new CommandLine(new Core(), new CommandFactory());
+    System.exit(commandLine.execute(args));
   }
 }
---
+++ app/src/main/java/io/github/syntaxpresso/core/util/CommandFactory.java
@@ -0,0 +1,21 @@
+package io.github.syntaxpresso.core.util;
+
+import io.github.syntaxpresso.core.command.java.CreateNewJavaFileCommand;
+import io.github.syntaxpresso.core.service.JavaService;
+import org.treesitter.TreeSitterJava;
+import picocli.CommandLine.IFactory;
+
+public class CommandFactory implements IFactory {
+  private final PathHelper pathHelper = new PathHelper();
+  private final TSHelper javaTsHelper = new TSHelper(new TreeSitterJava(), pathHelper);
+  private final JavaService javaService = new JavaService(pathHelper, javaTsHelper);
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public <K> K create(Class<K> cls) throws Exception {
+    if (cls == CreateNewJavaFileCommand.class) {
+      return (K) new CreateNewJavaFileCommand(javaService);
+    }
+    return cls.getDeclaredConstructor().newInstance();
+  }
+}
