62f096b46c90bc092e5a664ecd0728da0b9bedbf
André Luís <zandreluisos@gmail.com>
2025-07-26 17:36:19 -0300

    Adapt GetMainClassCommand to the refactor

--- app/src/main/java/io/github/syntaxpresso/core/command/java/GetMainClassCommand.java
+++ app/src/main/java/io/github/syntaxpresso/core/command/java/GetMainClassCommand.java
@@ -2,21 +2,20 @@ package io.github.syntaxpresso.core.command.java;
 
 import io.github.syntaxpresso.core.command.java.dto.GetMainClassResponse;
 import io.github.syntaxpresso.core.common.DataTransferObject;
-import io.github.syntaxpresso.core.service.java.JavaCodeAnalizerService;
-import io.github.syntaxpresso.core.service.java.JavaProjectStructureService;
+import io.github.syntaxpresso.core.service.JavaService;
 import java.io.File;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Callable;
+import lombok.RequiredArgsConstructor;
 import org.treesitter.TSTree;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
+@RequiredArgsConstructor
 @Command(name = "get-main-class", description = "Get Main class")
 public class GetMainClassCommand implements Callable<Void> {
-  private final JavaCodeAnalizerService javaCodeAnalizerService = new JavaCodeAnalizerService();
-  private final JavaProjectStructureService javaProjectStructureService =
-      new JavaProjectStructureService();
+  private final JavaService javaService;
 
   @Option(names = "--cwd", description = "Current Working Directory", required = true)
   private File cwd;
@@ -24,20 +23,20 @@ public class GetMainClassCommand implements Callable<Void> {
   @Override
   public Void call() throws Exception {
     GetMainClassResponse response = new GetMainClassResponse();
-    List<File> allFiles = this.javaProjectStructureService.findFilesByExtension(this.cwd, "java");
+    List<File> allFiles = this.javaService.getPathHelper().findFiles(this.cwd, "java");
     for (File file : allFiles) {
-      Optional<TSTree> tree = this.javaCodeAnalizerService.parse(file.toPath());
+      Optional<TSTree> tree = this.javaService.getTsHelper().parse(file);
       if (tree.isEmpty()) {
         continue;
       }
-      Optional<String> sourceCode = this.javaCodeAnalizerService.getSourceCode(file.toPath());
+      Optional<String> sourceCode = this.javaService.getPathHelper().getFileSourceCode(file);
       if (sourceCode.isEmpty()) {
         continue;
       }
-      boolean isMainClass = this.javaCodeAnalizerService.isMainClass(tree.get(), sourceCode.get());
+      boolean isMainClass = this.javaService.isMainClass(tree.get(), sourceCode.get());
       if (isMainClass) {
         Optional<String> packageName =
-            this.javaCodeAnalizerService.getPackageName(tree.get(), sourceCode.get());
+            this.javaService.getPackageName(tree.get(), sourceCode.get());
         if (packageName.isEmpty()) {
           System.out.println(DataTransferObject.error("Package name not found"));
           return null;
