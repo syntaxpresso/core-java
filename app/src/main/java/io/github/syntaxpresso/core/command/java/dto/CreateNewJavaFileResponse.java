130ee9e9a718f467efb1d56a51dad122347c4137
André Luís <zandreluisos@gmail.com>
2025-07-26 17:37:42 -0300

    Improve CreateNewJavaFileResponse

--- app/src/main/java/io/github/syntaxpresso/core/command/java/dto/CreateNewJavaFileResponse.java
+++ app/src/main/java/io/github/syntaxpresso/core/command/java/dto/CreateNewJavaFileResponse.java
@@ -1,29 +1,15 @@
 package io.github.syntaxpresso.core.command.java.dto;
 
 import java.io.Serializable;
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public final class CreateNewJavaFileResponse implements Serializable {
-  private final String content;
-
-  public CreateNewJavaFileResponse(String content) {
-    this.content = content;
-  }
-
-  public String getContent() {
-    return content;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (obj == this) return true;
-    if (obj == null || obj.getClass() != this.getClass()) return false;
-    var that = (CreateNewJavaFileResponse) obj;
-    return Objects.equals(this.content, that.content);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(content);
-  }
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateNewJavaFileResponse implements Serializable {
+  private String content;
 }
