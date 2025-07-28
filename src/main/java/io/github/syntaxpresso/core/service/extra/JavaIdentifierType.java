60ff417b6eef6f0d0086c509f8aa08a2cee06279
André Luís <zandreluisos@gmail.com>
2025-07-28 16:40:19 -0300

    Add JavaIdentifierType

---
+++ src/main/java/io/github/syntaxpresso/core/service/extra/JavaIdentifierType.java
@@ -0,0 +1,9 @@
+package io.github.syntaxpresso.core.service.extra;
+
+public enum JavaIdentifierType {
+  CLASS_NAME,
+  METHOD_NAME,
+  FIELD_NAME,
+  LOCAL_VARIABLE_NAME,
+  FORMAL_PARAMETER_NAME
+}
