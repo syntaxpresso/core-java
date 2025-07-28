91262f67ae5a38ac6ad045e3c815b7a7b33ce33f
André Luís <zandreluisos@gmail.com>
2025-07-28 20:45:19 -0300

    WIP

--- src/main/java/io/github/syntaxpresso/core/command/java/RenameCommand.java
+++ src/main/java/io/github/syntaxpresso/core/command/java/RenameCommand.java
@@ -1,16 +1,17 @@
 package io.github.syntaxpresso.core.command.java;
 
 import com.google.common.base.Strings;
+import io.github.syntaxpresso.core.common.TSFile;
+import io.github.syntaxpresso.core.common.extra.SupportedLanguage;
 import io.github.syntaxpresso.core.service.JavaService;
 import io.github.syntaxpresso.core.service.extra.JavaIdentifierType;
-import java.io.File;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Callable;
 import lombok.RequiredArgsConstructor;
 import org.treesitter.TSNode;
-import org.treesitter.TSTree;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
@@ -21,10 +22,10 @@ public class RenameCommand implements Callable<Void> {
   private final JavaService javaService;
 
   @Option(names = "--cwd", description = "Current Working Directory", required = true)
-  private File cwd;
+  private Path cwd;
 
   @Option(names = "--file-path", description = "The path to the file", required = true)
-  private File filePath;
+  private Path filePath;
 
   // @Option(names = "--line", description = "The line number of the symbol", required = true)
   // private int line;
@@ -37,40 +38,23 @@ public class RenameCommand implements Callable<Void> {
 
   @Override
   public Void call() {
-    List<File> modifiedFiles = new ArrayList<>();
-    File fileCopy = this.filePath;
-    Optional<TSTree> treeCopy = this.javaService.getTsHelper().parse(fileCopy);
-    if (!fileCopy.exists() || treeCopy.isEmpty()) {
-      System.out.println("1");
-      return null;
-    }
-    Optional<TSNode> node = this.javaService.getTsHelper().getNodeAtPosition(fileCopy, 7, 14);
-    if (node.isEmpty()) {
-      return null;
-    }
-    String currentName = this.javaService.getTsHelper().getIdentifierText(node.get(), fileCopy);
+    List<TSFile> modifiedFiles = new ArrayList<>();
+    TSFile file = new TSFile(SupportedLanguage.JAVA, this.filePath);
+    TSNode node = file.getNodeFromPosition(7, 14);
+    String currentName = file.getTextFromRange(node.getStartByte(), node.getEndByte());
     if (Strings.isNullOrEmpty(currentName)) {
       return null;
     }
-
-    JavaIdentifierType identifierType = this.javaService.getIdentifierType(node.get());
+    JavaIdentifierType identifierType = this.javaService.getIdentifierType(node);
     if (identifierType.equals(JavaIdentifierType.CLASS_NAME)) {
-      Optional<String> packageScopeName = this.javaService.getPackageName();
+      Optional<String> packageScopeName = this.javaService.getPackageName(file);
       if (packageScopeName.isEmpty()) {
         return null;
       }
-      boolean nodeRenamed =
-          this.javaService.getTsHelper().renameNode(this.filePath, node.get(), "NewName");
-      if (!nodeRenamed) {
-        return null;
-      }
-      File renamedFile =
-          this.javaService.getPathHelper().renameFile(this.filePath, "NewName" + ".java");
-      if (renamedFile == null) {
-        return null;
-      }
-      List<TSNode> usages = this.javaService.findClassUsages(currentName, this.cwd);
-      System.out.println(usages);
+      file.updateSourceCode(node, "NewName");
+      file.rename("NewName");
+      // List<TSNode> usages = this.javaService.findClassUsages(currentName, this.cwd);
+      // System.out.println(usages);
     }
 
     //
@@ -90,6 +74,7 @@ public class RenameCommand implements Callable<Void> {
     // System.out.println(methodIdentifierType);
     // System.out.println(localVariableIdentifierType);
     // System.out.println(formalParameterIdentifierType);
+    modifiedFiles.add(file);
     return null;
   }
 }
