7c448448159a4712729273ca3533a7414165943c
André Luís <zandreluisos@gmail.com>
2025-07-28 21:24:04 -0300

    Make commands automatically print call returns

--- src/main/java/io/github/syntaxpresso/core/Core.java
+++ src/main/java/io/github/syntaxpresso/core/Core.java
@@ -1,8 +1,8 @@
 package io.github.syntaxpresso.core;
 
-import io.github.syntaxpresso.core.common.CommandFactory;
 import io.github.syntaxpresso.core.command.GenericCommand;
 import io.github.syntaxpresso.core.command.JavaCommand;
+import io.github.syntaxpresso.core.common.CommandFactory;
 import picocli.CommandLine;
 
 @CommandLine.Command(
@@ -13,6 +13,11 @@ import picocli.CommandLine;
 public class Core {
   public static void main(String[] args) {
     CommandLine commandLine = new CommandLine(new Core(), new CommandFactory());
-    System.exit(commandLine.execute(args));
+    int exitCode = commandLine.execute(args);
+    Object executionResult = commandLine.getExecutionResult();
+    if (executionResult != null) {
+      System.out.println(executionResult);
+    }
+    System.exit(exitCode);
   }
 }
