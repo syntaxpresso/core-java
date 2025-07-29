cd96f2ce99520c0910d6e3c859a9760847c8e809
André Luís <zandreluisos@gmail.com>
2025-07-28 21:24:18 -0300

    Change GetMainClassCommand's return type

--- src/main/java/io/github/syntaxpresso/core/command/java/GetMainClassCommand.java
+++ src/main/java/io/github/syntaxpresso/core/command/java/GetMainClassCommand.java
@@ -5,6 +5,7 @@ import io.github.syntaxpresso.core.common.DataTransferObject;
 import io.github.syntaxpresso.core.common.TSFile;
 import io.github.syntaxpresso.core.common.extra.SupportedLanguage;
 import io.github.syntaxpresso.core.service.JavaService;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
@@ -15,15 +16,18 @@ import picocli.CommandLine.Option;
 
 @RequiredArgsConstructor
 @Command(name = "get-main-class", description = "Get Main class")
-public class GetMainClassCommand implements Callable<Void> {
+public class GetMainClassCommand implements Callable<DataTransferObject<GetMainClassResponse>> {
   private final JavaService javaService;
 
   @Option(names = "--cwd", description = "Current Working Directory", required = true)
   private Path cwd;
 
   @Override
-  public Void call() throws Exception {
-    GetMainClassResponse response = new GetMainClassResponse();
+  public DataTransferObject<GetMainClassResponse> call() throws Exception {
+    boolean cwdExists = Files.exists(this.cwd);
+    if (!cwdExists) {
+      throw new IllegalArgumentException("Current working directory does not exist.");
+    }
     List<TSFile> allFiles =
         this.javaService.getPathHelper().findFilesByExtention(this.cwd, SupportedLanguage.JAVA);
     for (TSFile file : allFiles) {
@@ -31,15 +35,16 @@ public class GetMainClassCommand implements Callable<Void> {
       if (isMainClass) {
         Optional<String> packageName = this.javaService.getPackageName(file);
         if (packageName.isEmpty()) {
-          System.out.println(DataTransferObject.error("Package name not found"));
-          return null;
+          return DataTransferObject.error(
+              "Main class found, but package name couldn't be determined.");
         }
+        GetMainClassResponse response = new GetMainClassResponse();
         response.setFilePath(file.getFile().getAbsolutePath());
         response.setPackageName(packageName.get());
-        System.out.println(DataTransferObject.success(response));
-        return null;
+        return DataTransferObject.success(response);
       }
     }
-    return null;
+    return DataTransferObject.error(
+        "Main class couldn't be found in the current working directory.");
   }
 }
